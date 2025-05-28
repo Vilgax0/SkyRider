6230977831a1ec43d7da7054d243c910cea8bcd2
Mkclaren <mkclaren.tobar@upb.edu.co>
2025-05-28 18:10:13 -0500

    Funcionalidad Menu
    Se agrega la navegacion entre los botones del menu

--- Scripts/mainmenu.gd
+++ Scripts/mainmenu.gd
@@ -1,1 +1,17 @@
 extends Control
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/level/level1.tscn")
+
+
+func _on_design_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/level/design.tscn")
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_check_button_pressed_mute() -> void:
+	get_tree().mute
